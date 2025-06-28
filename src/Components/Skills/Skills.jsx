e94f7623c482cb83520608c1ee0cded4e5c3ddb5
PxMach <moundjidmachghour58@gmail.com>
2025-06-28 14:32:48 +0100

    Refactor Skills component to use CSS modules for styling and remove unused icon link from index.html

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,25 +1,27 @@
+import styles from "./skills.module.css";
+
 export default function Skills() {
   return (
-    <section id="skills" class="section-skills">
+    <section id="skills" className={styles.sectionSkills}>
       <h2>My Skills</h2>
-      <div class="skills-container">
-        <div class="skill">
-          <i class="fa-brands fa-html5"></i>
+      <div className={styles.skillsContainer}>
+        <div className={styles.skill}>
+          <i className={`fa-brands fa-html5 ${styles.html5}`}></i>
           <h3>HTML/CSS</h3>
           <p>Elegant, responsive web page structures.</p>
         </div>
-        <div class="skill">
-          <i class="fa-brands fa-js"></i>
+        <div className={styles.skill}>
+          <i className={`fa-brands fa-js ${styles.js}`}></i>
           <h3>JavaScript</h3>
           <p>Development of interactive and dynamic functionalities.</p>
         </div>
-        <div class="skill">
-          <i class="fa-solid fa-database"></i>
+        <div className={styles.skill}>
+          <i className={`fa-solid fa-database ${styles.database}`}></i>
           <h3>MySQL</h3>
           <p>Efficient management of relational databases.</p>
         </div>
-        <div class="skill">
-          <i class="fa-brands fa-react"></i>
+        <div className={styles.skill}>
+          <i className={`fa-brands fa-react ${styles.react}`}></i>
           <h3>React</h3>
           <p>Building user interfaces with component-based architecture.</p>
         </div>
