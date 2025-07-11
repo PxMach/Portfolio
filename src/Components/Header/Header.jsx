562d4845c31a0b21cfe72d0bf0efeb594c52488c
PxMach <moundjidmachghour58@gmail.com>
2025-07-11 07:37:49 +0100

    Refactor Header component to improve menu toggle functionality and enhance styling for better user experience

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -25,31 +25,32 @@ export default function Header() {
         <div>
           <i
             onClick={handleClickOpen}
-            className={`fa-solid fa-bars ${styles.bars}`}
+            className={`fa-solid fa-bars ${styles.bars} 
+            ${showMenu ? styles.hiddeBars : ""}`}
           ></i>
         </div>
 
         {/* {showMenu && ( */}
-          <ul className={showMenu ? styles.showMenu : ""}>
-            <i
-              onClick={handleClickClose}
-              className={`fa-solid fa-xmark ${styles.close}`}
-            ></i>
-            <li>
-              <a href="#about" className={styles.active}>
-                About Me
-              </a>
-            </li>
-            <li>
-              <a href="#skills">Skills</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
+        <ul className={showMenu ? styles.showMenu : ""}>
+          <i
+            onClick={handleClickClose}
+            className={`fa-solid fa-xmark ${styles.close}`}
+          ></i>
+          <li>
+            <a href="#about" className={styles.active}>
+              About Me
+            </a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
         {/* )} */}
       </nav>
     </header>
