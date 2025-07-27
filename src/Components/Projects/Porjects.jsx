86b2cbd4c532fadb8f6b14701e62bafd81f8f7bc
PxMach <moundjidmachghour58@gmail.com>
2025-07-27 22:06:27 +0100

    Réorganiser et mettre à jour les détails des projets dans le composant Projects, en ajoutant de nouveaux éléments de projet et en améliorant la présentation pour une meilleure lisibilité.

--- src/Components/Projects/Porjects.jsx
+++ src/Components/Projects/Porjects.jsx
@@ -1,107 +1,112 @@
 import styles from "./projects.module.css";
 
 export default function Projects() {
-  return (
-    <section id="projects" className={styles.sectionProjects}>
-      <h2>Mes Projets</h2>
-      <div className={styles.projectList}>
-        <div className={styles.projectItem}>
-          <h3>
-            <a
-              href="https://pxmach.github.io/Frontend-Mentor-Newsletter-sign-up-form-with-success-message/"
-              target="_blank"
-            >
-              Newsletter sign-up form with success message
-            </a>
-          </h3>
-          <p>
-            A project that displays a newsletter sign-up form with a success
-            message upon submission.
-          </p>
-          <span>Technologies used : HTML, CSS, JavaScript</span>
-        </div>
+   return (
+      <section id="projects" className={styles.sectionProjects}>
+         <h2>Mes Projets</h2>
+         <div className={styles.projectList}>
+            <div className={styles.projectItem}>
+               <h3>
+                  <a
+                     href="https://pxmach.github.io/Frontend-Mentor-Newsletter-sign-up-form-with-success-message/"
+                     target="_blank"
+                  >
+                     Newsletter sign-up form with success message
+                  </a>
+               </h3>
+               <p>
+                  A project that displays a newsletter sign-up form with a
+                  success message upon submission.
+               </p>
+               <span>Technologies used : HTML, CSS, JavaScript</span>
+            </div>
 
-        <div className={styles.projectItem}>
-          <h3>
-            <a
-              href="https://frontend-mentor-huddle-landing-page-lovat.vercel.app/"
-              target="_blank"
-            >
-              Huddle landing page with single introductory section
-            </a>
-          </h3>
-          <p>
-            A landing page for Huddle with a single introductory section,
-            showcasing the platform's features and benefits.
-          </p>
-          <span>
-            Technologies used : HTML, CSS, JavaScript
-            <code>(React, Tailwind CSS)</code>
-          </span>
-        </div>
+            <div className={styles.projectItem}>
+               <h3>
+                  <a
+                     href="https://frontend-mentor-intro-component-with-sign-up-form-n4ypd6cd3.vercel.app/"
+                     target="_blank"
+                  >
+                     Intro component with sign up form
+                  </a>
+               </h3>
+               <p>
+                  A component that displays an introduction section with a
+                  sign-up form, featuring form validation and responsive design.
+               </p>
+               <span>
+                  Technologies used : HTML, CSS, JavaScript{" "}
+                  <code>(Next.js(React), Tailwind CSS)</code>
+               </span>
+            </div>
 
-        <div className={styles.projectItem}>
-          <h3>
-            <a
-              href="https://pxmach.github.io/Frontend-Mentor-Loopstudios-landing-page/"
-              target="_blank"
-            >
-              Loopstudios landing page
-            </a>
-          </h3>
-          <p>
-            A responsive landing page for Loopstudios showcasing their services
-            and projects.
-          </p>
-          <span>Technologies used : HTML, CSS, JavaScript</span>
-        </div>
-        <div className={styles.projectItem}>
-          <h3>
-            <a
-              href="https://pxmach.github.io/Frontend-Mentor-Bookmark-landing-page/"
-              target="_blank"
-            >
-              Bookmark landing page
-            </a>
-          </h3>
-          <p>
-            A bookmark landing page to save and manage your favorite websites.
-          </p>
-          <span>Technologies used : HTML, CSS, JavaScript</span>
-        </div>
-        <div className={styles.projectItem}>
-          <h3>
-            <a
-              href="https://pxmach.github.io/Frontend-Mentor-Tip-calculator-app/"
-              target="_blank"
-            >
-              Tip calculator app
-            </a>
-          </h3>
-          <p>
-            A tip calculator app to help users calculate the appropriate tip
-            amount based on the bill total and desired tip percentage.
-          </p>
-          <span>Technologies used : HTML, CSS, JavaScript</span>
-        </div>
+            <div className={styles.projectItem}>
+               <h3>
+                  <a
+                     href="https://frontend-mentor-huddle-landing-page-lovat.vercel.app/"
+                     target="_blank"
+                  >
+                     Huddle landing page with single introductory section
+                  </a>
+               </h3>
+               <p>
+                  A landing page for Huddle with a single introductory section,
+                  showcasing the platform's features and benefits.
+               </p>
+               <span>
+                  Technologies used : HTML, CSS, JavaScript{" "}
+                  <code>(React, Tailwind CSS)</code>
+               </span>
+            </div>
 
-        <div className={styles.projectItem}>
-          <h3>
-            <a
-              href="https://pxmach.github.io/Frontend-Mentor-E-commerce-product-page/"
-              target="_blank"
-            >
-              E-commerce product page
-            </a>
-          </h3>
-          <p>
-            A product page for an e-commerce website showcasing a specific item
-            with images, descriptions, and purchase options.
-          </p>
-          <span>Technologies used : HTML, CSS, JavaScript</span>
-        </div>
+            <div className={styles.projectItem}>
+               <h3>
+                  <a
+                     href="https://pxmach.github.io/Frontend-Mentor-Bookmark-landing-page/"
+                     target="_blank"
+                  >
+                     Bookmark landing page
+                  </a>
+               </h3>
+               <p>
+                  A bookmark landing page to save and manage your favorite
+                  websites.
+               </p>
+               <span>Technologies used : HTML, CSS, JavaScript</span>
+            </div>
+            <div className={styles.projectItem}>
+               <h3>
+                  <a
+                     href="https://pxmach.github.io/Frontend-Mentor-Tip-calculator-app/"
+                     target="_blank"
+                  >
+                     Tip calculator app
+                  </a>
+               </h3>
+               <p>
+                  A tip calculator app to help users calculate the appropriate
+                  tip amount based on the bill total and desired tip percentage.
+               </p>
+               <span>Technologies used : HTML, CSS, JavaScript</span>
+            </div>
 
-        {/* <div className={styles.projectItem}>
+            <div className={styles.projectItem}>
+               <h3>
+                  <a
+                     href="https://pxmach.github.io/Frontend-Mentor-E-commerce-product-page/"
+                     target="_blank"
+                  >
+                     E-commerce product page
+                  </a>
+               </h3>
+               <p>
+                  A product page for an e-commerce website showcasing a specific
+                  item with images, descriptions, and purchase options.
+               </p>
+               <span>Technologies used : HTML, CSS, JavaScript</span>
+            </div>
+
+            {/* <div className={styles.projectItem}>
           <h3>
             <a href="#" target="_blank">
               Product list with cart
@@ -117,25 +122,24 @@ export default function Projects() {
           </span>
         </div> */}
 
-        <div className={styles.projectItem}>
-          <h3>
-            <a
-              href="https://frontend-mentor-fylo-landing-page-w.vercel.app/"
-              target="_blank"
-            >
-              Fylo landing page with dark theme and features grid
-            </a>
-          </h3>
-          <p>
-            A landing page for Fylo with a dark theme and a grid layout
-            showcasing its features.
-          </p>
-          <span>
-            Technologies used : HTML, CSS, JavaScript
-            <code>(React)</code>
-          </span>
-        </div>
-      </div>
-    </section>
-  );
+            <div className={styles.projectItem}>
+               <h3>
+                  <a
+                     href="https://frontend-mentor-fylo-landing-page-w.vercel.app/"
+                     target="_blank"
+                  >
+                     Fylo landing page with dark theme and features grid
+                  </a>
+               </h3>
+               <p>
+                  A landing page for Fylo with a dark theme and a grid layout
+                  showcasing its features.
+               </p>
+               <span>
+                  Technologies used : HTML, CSS, JavaScript <code>(React)</code>
+               </span>
+            </div>
+         </div>
+      </section>
+   );
 }
